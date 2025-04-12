a2a679c05610790595e693b8a7a1f1100705c41a
Bhavishya <121146811+bhavishya2324@users.noreply.github.com>
2025-04-12 22:32:13 +0530

    Update banner.tsx

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Cpu, Lock, Sparkles, Zap } from 'lucide-react'
+
 
 export default function ContentSection3() {
     return (
@@ -8,7 +8,7 @@ export default function ContentSection3() {
             <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-12">
                 <div className="mx-auto max-w-xl space-y-6 text-center md:space-y-12">
                     <h2 className="text-balance text-3xl font-medium lg:text-4xl">Scale Without Limits: Transform Your Business with Growton 🔥</h2>
-                    <p>At Growton, we don't just improve metrics—we engineer exponential growth. Our proprietary methodology combines data-driven decision making with creative strategy to identify and exploit your untapped growth opportunities.</p>
+                    <p>At Growton, we do not just improve metrics—we engineer exponential growth. Our proprietary methodology combines data-driven decision making with creative strategy to identify and exploit your untapped growth opportunities.</p>
                 </div>
                 <img className="rounded-(--radius) grayscale" src="./banner.jpeg" alt="team image" height="" width="" loading="lazy" />
 
